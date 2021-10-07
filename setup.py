9c56883216998f3531f22a8cfcefd04669275e18
jytan17 <87595774+jytan17@users.noreply.github.com>
2021-10-07 01:57:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setuptools.setup(
     version='0.0.1',
     author='Junyong Tan',
     author_email='jtan9801@gmail.com',
-    description='A small machine learning package',
-    url='https://github.com/jytan17/deeplearning_framework',
+    description='A small deep learning package',
+    url='https://github.com/jytan17/deep_learning_framework',
     project_urls = {
-        "Bug Tracker": "https://github.com/jytan17/deeplearning_framework/issues"
+        "Bug Tracker": "https://github.com/jytan17/deep_learning_framework/issues"
     },
     license='MIT',
     packages=['models'],
